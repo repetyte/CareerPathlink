ed96ff111b2c373a6f605e9efe7e57889ba5b29d
repetyte <hendrixonmoldes23@gmail.com>
2024-11-05 00:07:39 +0800

    Minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Recruitment and Placements',
+      title: 'Recruitment and Placement',
       theme: AppTheme.themeData,
       // home: const Dashboard(),
       home: const RrJobDashboardUser(),
